9977d10d43e3fa67839552d44d49f97bca561fa4
vignesha399 <vignesha399>
2021-12-29 13:32:32 +0530

    commit on main

---
+++ Map.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Map {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1", "value no 1");
+        map.put("2", "value no 2");
+        System.out.println(map);
+        map.forEach((a, b) -> System.out.println("Map is :"+ a +" : "+ b));
+    }
+}
